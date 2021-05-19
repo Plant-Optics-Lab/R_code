9d87a612579b97243f0645506c9ed00f28410852
Jessie Au <jessieau@ucdavis.edu>
2021-05-19 15:34:34 -0700

    Adding introductions

---
+++ Leaf_level/Li600_wrangle.R
@@ -0,0 +1,3 @@
+# Welcome to a script for the Li-600: importing, wrangling and exporting the data. 
+
+# We use the Li-600 to get plant physiology information. We use it to collect measurements on stomatal conductance and fluorescence. There two types of fluorescence measurements, one collected during the daytime(light-adapted leaf) and one collected during the night (dark adapted leaf). This script will be used for importing the raw data, merging it with meta data files and exporting it for analyses. 

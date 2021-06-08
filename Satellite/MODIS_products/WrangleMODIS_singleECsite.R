25faf8d1a96397080506e21da21d3b5079173e51
Jessie Au <jessieau@ucdavis.edu>
2021-06-07 17:29:15 -0700

    added instructions for getting this data

--- Satellite/MODIS_products/WrangleMODIS_singleECsite.R
+++ Satellite/MODIS_products/WrangleMODIS_singleECsite.R
@@ -1,1 +1,6 @@
-#This script is for wrangling MODIS data collected from tutorial "HowToAccessMODISdata". 
+#This script is for wrangling MODIS data collected from tutorial "HowToAccessMODISdata". Brief instructions will be provided for collecting the data from a server. 
+# 1. Go to https://modis.ornl.gov/data.html
+# 2a. For sites at a designated Eddy Covariance site: click on "Fixed Sites Subsets Tool".
+# 2b. To enter coordinates for study area: click on “Global Subsets Tool”.
+
+
